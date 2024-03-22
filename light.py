6b6c179e18b7e88bb5826acd6eea2b4d2e9fca14
emilyslane24 <erslane2005@gmail.com>
2024-03-21 19:33:37 -0700

    asdf

---
+++ light.py
@@ -0,0 +1,15 @@
+import machine
+import utime
+
+#connecting to machine
+led_onboard = machine.Pin(10, machine.Pin.OUT)
+sensor = machine.ADC(28)
+conversion_factor = 3.3/(65535)
+
+#turning led on/off based on readings
+while True:
+    reading = sensor.read_u16()*conversion_factor
+    if (reading > 3.0):
+        led_onboard.value(1)
+    else:
+        led_onboard.value(0)
