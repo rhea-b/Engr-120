d2e3351c29e55af0f25bbe9d3eebe964a6bc3f70
Adeline Theodora <adeline.theodora.stu@elyon.sch.id>
2024-03-21 19:33:12 -0700

    dsa

--- thermistorcode.py
+++ thermistorcode.py
@@ -18,11 +18,11 @@ Ro = 10000
 
 #loop to determine which components is turning on
 while True:
-    Vout = temperature_sensor.read_u16()*conversion_factor
-    Rt = (Vout * Ro)/(Vin - Vout)
-    if (Rt > 10000): #10k Rt should be equal to around 25 degrees celcius
-        led_onboard1.value(1)
-        led_onboard2.value(0)
+    Vout = temperature_sensor.read_u16()*conversion_factor #getting voltage out 
+    Rt = (Vout * Ro)/(Vin - Vout) #calculating resistance
+    if (Rt > 10000): #10k Rt should be equal to around 25 degrees celcius, if more heater should turn on and fan turn off
+        led_onboard1.value(1) #heater on
+        led_onboard2.value(0) #fan off
     else:
-        led_onboard1.value(0)
-        led_onboard2.value(1)
+        led_onboard1.value(0) #heater off
+        led_onboard2.value(1) #fan on
